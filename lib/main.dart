43fe4bc3418073d2c1f1566858c5b71404fd2ecc
Ziggy010 <tajale01@gmail.com>
2022-10-02 19:51:26 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text(
+            'Flutter catalog app',
+          ),
+        ),
+      ),
+    );
+  }
+}
